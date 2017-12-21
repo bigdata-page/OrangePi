456981f635580c7ac7e136b9ee14baa88a0ea1ae
liulianjun <lj879933274@163.com>
2017-12-21 14:13:10 +0800

    add the marco definition of usartt

--- src/LOCK/send_data.h
+++ src/LOCK/send_data.h
@@ -7,8 +7,13 @@
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
-#define SEND_DATA_LENGTH 5
-#define RS485_EN	21
+#define SEND_DATA_LENGTH 	5
+#define RS485_EN			21
+#define Raspberry_USART0   	"/dev/ttyAMA0"
+
+#define OrangePI_USART0 	"/dev/ttyS0"
+#define OrangePI_USART1 	"/dev/ttyS1"
+#define OrangePI_USART2 	"/dev/ttyS2"
 
 
 int send_data(int fd, unsigned char data[]);
